44c8c5a95ad12cd0275f093822626cbd49718db1
Rashid J. Almheiri <r.muhairi@pm.me>
2024-10-01 20:55:09 +0400

    Cloudflare Pages deployment +redirect rules

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -27,14 +27,6 @@ resource "cloudflare_record" "codershq_tunnel_cname" {
   proxied = true
 }
 
-resource "cloudflare_record" "golang_tunnel_cname" {
-  zone_id = local.golang.zone_id
-  name    = "@"
-  content = local.uaq_tunnel.cname
-  type    = "CNAME"
-  proxied = true
-}
-
 resource "cloudflare_record" "golang_github_verification" {
   zone_id = local.golang.zone_id
   name = "_gh-GoUAE-o"
@@ -91,16 +83,111 @@ resource "cloudflare_email_routing_rule" "cm-golang-ae" {
   }
 }
 
+resource "cloudflare_list" "gouae_social_redirect_list" {
+  account_id  = local.account_id
+  name        = "gouae_social_redirect_list"
+  description = "Social Redirect list for GoUAE"
+  kind        = "redirect"
+
+  item {
+    value {
+      redirect {
+        source_url = "golang.ae/discord"
+        target_url = "https://discord.gg/FXhjYCkvGg"
+      }
+    }
+  }
+
+  item {
+    value {
+      redirect {
+        source_url = "golang.ae/whatsapp"
+        target_url = "https://chat.whatsapp.com/DEmS5AmfJSfBmH40aYLih1"
+      }
+    }
+  }
+
+  item {
+    value {
+      redirect {
+        source_url = "golang.ae/twitter"
+        target_url = "https://x.com/uaegophers"
+      }
+    }
+  }
+
+  item {
+    value {
+      redirect {
+        source_url = "golang.ae/linkedin"
+        target_url = "https://linkedin.com/company/gouae"
+      }
+    }
+  }
+
+  item {
+    value {
+      redirect {
+        source_url = "golang.ae/github"
+        target_url = "https://github.com/gouae"
+      }
+    }
+  }
+}
+
+resource "cloudflare_ruleset" "gouae_social_redirect_ruleset" {
+  account_id  = local.account_id
+  name        = "redirects"
+  description = "GoUAE Socials Redirect ruleset"
+  kind        = "root"
+  phase       = "http_request_redirect"
+
+  rules {
+    action = "redirect"
+    action_parameters {
+      from_list {
+        name = "gouae_social_redirect_list"
+        key = "http.request.full_uri"
+      }
+    }
+
+    expression = "http.request.full_uri in $gouae_social_redirect_list"
+    description = "Apply redirects from gouae_social_redirect_list"
+    enabled = true
+  }
+}
+
+
+/* Equivalent to what we're doing on cloudflare, but 
+  fails to work with terraform due to
+ `There is an internal issue with your Cloudflare Pages Git installation` */
+
 # resource "cloudflare_pages_project" "golang_ae_site" {
 #   account_id        = local.account_id
-#   name              = "this-is-my-project-01"
+#   name              = "golang-ae"
 #   production_branch = "main"
-#   source = [{
+
+#   source { 
 #     type = "github"
-#     config = {
+
+#     config {
 #       owner = "gouae"
 #       repo_name = "golang.ae"
 #       production_branch = "main"
 #     }    
-#   }]
+#   }
+
+#   build_config {
+#     build_command   = "go run main.go --static"
+#     destination_dir = ""
+#     root_dir        = ""
+#   }
+# }
+
+# resource "cloudflare_record" "golang_pages_cname" {
+#   zone_id = local.golang.zone_id
+#   name    = "@"
+#   content = "golang-ae.pages.dev"
+#   type    = "CNAME"
+#   proxied = true
 # }
